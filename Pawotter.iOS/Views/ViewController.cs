5db11b1348846bf230211c62b6fc61c1b83e25a6
gomi_ningen <korun24276@gmail.com>
2017-05-02 01:19:57 +0900

    Goodbye Storyboard

---
+++ Pawotter.iOS/Views/ViewController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using UIKit;
+
+namespace Pawotter.iOS
+{
+    public class ViewController : UIViewController
+    {
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            View.BackgroundColor = UIColor.White;
+        }
+    }
+}
